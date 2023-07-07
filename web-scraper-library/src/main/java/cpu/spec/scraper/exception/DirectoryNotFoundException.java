b3180b157e41e3ae4226604a73a9e9d9aa0c1f8d
felixsteinke <steinke.felix@yahoo.de>
2023-07-07 12:09:14 +0200

    update documentation

--- web-scraper-library/src/main/java/cpu/spec/scraper/exception/DirectoryNotFoundException.java
+++ web-scraper-library/src/main/java/cpu/spec/scraper/exception/DirectoryNotFoundException.java
@@ -2,8 +2,11 @@ package cpu.spec.scraper.exception;
 
 import cpu.spec.scraper.utils.FileUtils;
 
+/**
+ * Exception with appropriate message.
+ */
 public class DirectoryNotFoundException extends Exception {
     public DirectoryNotFoundException(String directoryName) {
-        super("Directory ()" + directoryName + ") not found in execution directory: " + FileUtils.getExecutionDirectory());
+        super("Directory (" + directoryName + ") not found in execution directory: " + FileUtils.getExecutionDirectory());
     }
 }
--- web-scraper-library/src/main/java/cpu/spec/scraper/exception/ElementNotFoundException.java
+++ web-scraper-library/src/main/java/cpu/spec/scraper/exception/ElementNotFoundException.java
@@ -1,5 +1,8 @@
 package cpu.spec.scraper.exception;
 
+/**
+ * Exception with appropriate message.
+ */
 public class ElementNotFoundException extends Exception {
     public ElementNotFoundException(String rootElement, String query) {
         super("Element in '" + rootElement + "' not found with query: '" + query + "'");
